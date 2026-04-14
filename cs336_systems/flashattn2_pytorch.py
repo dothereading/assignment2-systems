3856e2e329b0a6eefd050203917e2b4d7fd11121
dothereading <miguelconner4@gmail.com>
2026-04-14 15:54:50 -0400

    first full draft of pytorch flashattn2

--- cs336_systems/flashattn2_pytorch.py
+++ cs336_systems/flashattn2_pytorch.py
@@ -1,51 +1,70 @@
 import torch
+import math
+from einops import einsum
 
+B_k = 16
+B_q = 16
 
-B_c = 16
-B_r = 16
-
-class FlassAttn2(torch.autograd.Function):
+class FlashAttn2(torch.autograd.Function):
 
     @staticmethod
     def forward(ctx, K: torch.Tensor, Q: torch.Tensor, V: torch.Tensor, is_causal: bool = False):
         
-        _, N, d = K.size() # N is seq_len, d is dimensions
+        batch, N, d = K.size() # _ is batch, N is seq_len, d_k is dimensions
+        _, _, d_v = V.size() # _ is batch, N is seq_len, d_v is dimensions
 
-        T_r = (N + B_r - 1) // B_r # formula for cieling division
-        T_c  = (N + B_c - 1) // B_c
 
-        Q_list = torch.split(Q, T_r, dim=2)
-        K_list = torch.split(K, T_c, dim=2)
-        V_list = torch.split(V, T_c, dim=2)
+        T_q = (N + B_q - 1) // B_q # formula for cieling division
+        T_k  = (N + B_k - 1) // B_k
 
-        for i in range(T_r):
+        Q_list = torch.split(Q, B_q, dim=1)
+        K_list = torch.split(K, B_k, dim=1)
+        V_list = torch.split(V, B_k, dim=1)
+        O_list = []
+        L_list = []
+
+        for i in range(T_q):
             Q_i = Q_list[i]
 
             O_i = torch.zeros_like(Q_i)
-            l_i = torch.zeros(B_r)
-            m_i = torch.full((1, B_r), -float('inf'))
+            l_i = torch.zeros(batch, B_q)
+            m_i = torch.full((batch, B_q), -float('inf'))
 
-            for j in range(T_c):
+            for j in range(T_k):
                 K_j = K_list[j]
                 V_j = V_list[j]
 
-                S_ij = Q_i @ K_j.T
-                m_ij = torch.max(m_i[j-1 if j>0 else 0], max(S_ij))
-                P_i = #bla
-                l_i = #bla
-                O_i = #bla
+                S_ij = einsum(Q_i, K_j, "... query d_k, ... key d_k -> ... query key") / math.sqrt(d)
+                m_i_prev = m_i
+                print("S_ij\n\n")
+                print(S_ij.shape)
+                print(f"Dimension of max {torch.max(S_ij, dim=-1).values.shape}")
+                print(f"Dimension of m_i {m_i.shape}")
 
-                # end for...
-            # end for...
+                m_i = torch.maximum(m_i, torch.max(S_ij, dim=-1).values)
+                P_i = torch.exp(S_ij - torch.unsqueeze(m_i, dim=-1)) # dimensions are sus
+                l_i = torch.exp(m_i_prev - m_i) * l_i + torch.sum(P_i, dim=-1) # dim still sus
+                O_i = torch.exp(m_i_prev - m_i).unsqueeze(dim=-1) * O_i + einsum(P_i, V_j, "... seq seq, ... seq d_v -> ... seq d_v")
 
+            O_i = O_i / l_i.unsqueeze(-1)
+            L_i = m_i + torch.log(l_i)
+
+            O_list.append(O_i)
+            L_list.append(L_i)
+
+        O = torch.cat(O_list, dim=1)
+        L = torch.cat(L_list, dim=1)
+
+        print(f"L dimensions: {L.shape}")
+        print(f"O dimensions: {O.shape}")
 
         ctx.save_for_backward(L, Q, K, V, O)
         ctx.is_causal = is_causal
-        return torch.cat(O_i, dim=2)
+        return O
 
     @staticmethod
     def backward(ctx, grad_out):
         raise NotImplementedError
     
-# O = FlashAttn2.apply(Q, K, V
+# O = FlashAttn2.apply(Q, K, V)
     
--- tests/adapters.py
+++ tests/adapters.py
@@ -4,6 +4,8 @@ from typing import Type
 
 import torch
 
+from cs336_systems.flashattn2_pytorch import FlashAttn2
+
 
 
 def get_flashattention_autograd_function_pytorch() -> Type:
@@ -15,8 +17,7 @@ def get_flashattention_autograd_function_pytorch() -> Type:
     Returns:
         A class object (not an instance of the class)
     """
-    # For example: return MyFlashAttnAutogradFunctionClass
-    raise NotImplementedError
+    return FlashAttn2
 
 
 def get_flashattention_autograd_function_triton() -> Type:
